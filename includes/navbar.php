6185db494ab81219b0e4596af1b31672c07dfc13
Avik Nigam <business.aviknigam@gmail.com>
2017-10-22 22:40:19 +1100

    Removed bootstrap from navbar

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,10 +1,10 @@
-<nav class="navbar navbar-expand-md navbar-light bg-light">
-    <a class="navbar-brand mr-md-4" href="/">
-        <img class="nav-logo" src="/assets/images/logo.png" alt="FITTBLOG">
+<div class="bs-nav">
+    <a class="nav-bs-brand" href="/">
+        <img class="nav-bs-logo" src="/assets/images/logo.png" alt="FITTBLOG">
     </a>
     
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
+    <!-- <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
-    </button>
+    </button> -->
 
-</nav>
+</div>
