d2baab2681ee9f84b208171afd63861698422d11
Avik Nigam <business.aviknigam@gmail.com>
2017-12-14 12:32:43 +1100

    Navbar updated to remove bootstrap

--- includes/navbar.php
+++ includes/navbar.php
@@ -2,9 +2,4 @@
     <a class="nav-bs-brand" href="/">
         <img class="nav-bs-logo" src="/assets/images/logo.png" alt="FITTBLOG">
     </a>
-    
-    <!-- <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button> -->
-
 </div>
