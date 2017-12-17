2ea41c182cf1833fb9e8fcf58e45497470d7b472
Avik Nigam <business.aviknigam@gmail.com>
2017-12-18 04:57:53 +1100

    Fixed sidebar

--- includes/content.php
+++ includes/content.php
@@ -1,14 +1,14 @@
 <div class="fb-container flex">
     <!-- Sidebar -->
         <div class="fb-sidebar flex">
-            <div class="fb-sidebar-links flex">
-                <p>Follow us on </p>
-                <a href="https://www.instagram.com/aviknigam/"><i class="fab fa-instagram fa-3x flex"></i></a>
-                <a href="https://www.instagram.com/aviknigam/"><i class="fab fa-facebook-square fa-3x flex"></i></a>
-                <a href="/fitness">Fitness</a>
-                <a href="/health">Health</a>
-                <a href="/nutrition">Nutrition</a>
-            </div>
+            <ul>
+                <span>Follow us on </span>
+                <li><a href="https://www.instagram.com/aviknigam/"><i class="fab fa-instagram fa-3x"></i></a></li>
+                <li><a href="https://www.instagram.com/aviknigam/"><i class="fab fa-facebook-square fa-3x"></i></a></li>
+                <li><a href="/fitness">Fitness</a></li>
+                <li><a href="/health">Health</a></li>
+                <li><a href="/nutrition">Nutrition</a></li>
+            </ul>
         </div>
 
     <!-- Main Content -->
@@ -34,7 +34,7 @@
                                 "</a>
                                 <div class='fb-card-body'>
                                     <h4 class='fb-card-title'><a href='/$row[postCategory]/$row[postSlug]'>$row[postTitle]</a></h4>
-                                    <p class='fb-card-text'>" .substr($row['postDescription'], 0, 110) ." ...</p>
+                                    <p class='fb-card-text'>" .substr($row['postDescription'], 0, 110) ."...</p>
                                     <a href='/$row[postCategory]/$row[postSlug]'><u>Read more...</u></a>
                                 </div>
                             </div>";
