f203dd5be092127a7046e882d73c3168f68059aa
Avik Nigam <business.aviknigam@gmail.com>
2017-12-18 05:26:49 +1100

    added sidebar.php to includes

--- includes/content.php
+++ includes/content.php
@@ -1,15 +1,6 @@
 <div class="fb-container flex">
     <!-- Sidebar -->
-        <div class="fb-sidebar flex">
-            <ul>
-                <span>Follow us on </span>
-                <li><a href="https://www.instagram.com/aviknigam/"><i class="fab fa-instagram fa-3x"></i></a></li>
-                <li><a href="https://www.instagram.com/aviknigam/"><i class="fab fa-facebook-square fa-3x"></i></a></li>
-                <li><a href="/fitness">Fitness</a></li>
-                <li><a href="/health">Health</a></li>
-                <li><a href="/nutrition">Nutrition</a></li>
-            </ul>
-        </div>
+        <?php include('sidebar.php'); ?>
 
     <!-- Main Content -->
         <div class="fb-main-content flex">
