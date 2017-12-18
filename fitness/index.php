ce1594b1d42e1ed27c029be2c063c9ba774dad8f
Avik Nigam <business.aviknigam@gmail.com>
2017-12-19 02:05:58 +1100

    Completed MVP design of front page and viewposts

--- fitness/index.php
+++ fitness/index.php
@@ -1,23 +1,4 @@
 <?php
 require __DIR__ . '/../core/init.php';
-
-$urlSlug = sanitize($_GET['slug']);
-
-// Prepare the statement -> not querying just yet
-$stmt = $conn->prepare('SELECT * FROM posts WHERE postSlug = ?');
-
-// Bind the parameters
-$stmt->bind_param("s", $postSlug);
-$postSlug = $urlSlug;
-
-// Execute query
-$stmt->execute();
-
-// Need get_result() to give the result to
-$result = $stmt->get_result();
-
-// Exit if we cannot find the post in SQL
-if(!$result->num_rows > 0) {
-    header("Location: /");
-    exit;
-}
+$category = "fitness";
+include '../includes/category-content.php';
--- index.php
+++ index.php
@@ -34,7 +34,43 @@ $description = "Health and fitness stories from around the world. Inspirational 
 			<?php include('includes/navbar.php'); ?>
 		
 		<!-- Main Content -->
-			<?php include('includes/content.php') ?>
+			<div class="fb-container flex">
+				<!-- Sidebar -->
+					<?php include 'includes/sidebar.php'; ?>
+
+				<!-- Main Content -->
+					<div class="fb-main-content flex">
+						<h1 class="fb-heading">Latest Posts</h1>
+							<div class="fb-posts flex">
+								<?php
+
+								$stmt = $conn->query('SELECT * FROM posts ORDER BY postID desc');
+
+								while($row = $stmt->fetch_assoc()) {
+									echo 
+										"<div class='fb-card'>
+											<a href='/$row[postCategory]/$row[postSlug]'>";
+									
+									if(empty($row['postImage'])) {
+										echo "<img src='/assets/images/$row[postID].jpg'>";
+									} else {
+										echo "<img src='$row[postImage]'>";
+									}
+									
+									echo
+											"</a>
+											<div class='fb-card-body'>
+												<h4 class='fb-card-title'><a href='/$row[postCategory]/$row[postSlug]'>$row[postTitle]</a></h4>
+												<p class='fb-card-text'>" .substr($row['postDescription'], 0, 110) ."...</p>
+												<a href='/$row[postCategory]/$row[postSlug]'><u>Read more...</u></a>
+											</div>
+										</div>";
+								};
+
+								?>
+							</div>
+					</div>
+			</div>
 		
 		<!-- Footer -->
 			<?php include('includes/footer.php'); ?>
@@ -43,6 +79,3 @@ $description = "Health and fitness stories from around the world. Inspirational 
 			<?php include('includes/scripts.php'); ?>
 	</body>
 </html>
-
-<!-- category=$row[postCategory]&title=$row[postTitle]&
-category=$row[postCategory]&title=$row[postTitle]& -->
