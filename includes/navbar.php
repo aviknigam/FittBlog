fdc98fef5a31d4406686619b1570edc013b714b5
Avik Nigam <business.aviknigam@gmail.com>
2017-12-26 09:01:04 +1100

    v2 navbar with date

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,5 +1,16 @@
-<div class="fb-nav flex">
-    <a href="/">
-        <img class="fb-nav-logo" src="/assets/images/logo.png" alt="FITTBLOG">
-    </a>
+<div class="navbar" itemscope itemtype="http://schema.org/WPHeader">
+    <div class="masthead flex">
+        <span><i class="fas fa-caret-right fa-2x"></i></span>
+        <span class="date">
+            <?php
+            date_default_timezone_set('Australia/Sydney');
+            echo date('jS F Y', strtotime("now")); 
+            ?>
+        </span>
+    </div>
+    <div class="nav-logo flex">
+        <a href="/">
+            <img src="/assets/images/logo.png" alt="FITTBLOG">
+        </a>
+    </div>
 </div>
